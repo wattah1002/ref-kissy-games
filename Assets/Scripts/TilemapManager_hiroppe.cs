31d936fd688c47542bfaa3bda95833ef6a84bfc5
matsuba9393 <cymbal0515paiste@gmail.com>
2023-03-21 12:47:59 +0900

    2画面目のタイルマップを追加,タイルマップ制御のスクリプトを作成

---
+++ Assets/Scripts/TilemapManager_hiroppe.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilemapManager_hiroppe : MonoBehaviour
+{
+    void Start(){
+        
+    }
+
+    void Update(){
+        
+    }
+}
