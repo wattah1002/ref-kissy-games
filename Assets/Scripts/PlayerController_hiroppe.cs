a388bd6b5eeee04362df5a29397539a551f7bd55
matsuba9393 <cymbal0515paiste@gmail.com>
2023-03-21 22:16:12 +0900

    画面遷移を実装

--- Assets/Scripts/PlayerController_hiroppe.cs
+++ Assets/Scripts/PlayerController_hiroppe.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController_hiroppe : MonoBehaviour
 {
     private Animator anim;
     private Rigidbody2D rb;
@@ -14,23 +14,31 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private bool isGround = true;
     
+    public GameObject _camera;
+    private float[] leftSide = {-230,250};
+    private int fieldNum = 0;
+
+    private bool fieldFlag = true;
+    private bool moveFlag = true;
 
 
     void Start(){
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        
     }
 
     void Update(){
         PlayerMoveHorizontal();
         PlayerMoveJump();
+        WaitProcess();
     }
 
     void PlayerMoveHorizontal(){
         //左右移動
         move = Input.GetAxis("Horizontal");
-        if(transform.position.x > -230){
-            if(move != 0){
+        if(transform.position.x > leftSide[fieldNum] && transform.position.x < 720){
+            if(move != 0 && moveFlag){
                 if(move >= 0) move =1;
                 else move = -1;
 
@@ -42,16 +50,33 @@ public class PlayerController : MonoBehaviour
                 anim.SetBool("isWalk",false);
             }
         }
-        else transform.position = new Vector3(-229, transform.position.y, 1);
+        else transform.position = new Vector3(leftSide[fieldNum] + 1, transform.position.y, 1);
     }
 
     void PlayerMoveJump(){
-        if(isGround&&(Input.GetKeyDown("up")||Input.GetKeyDown(KeyCode.Space))){
+        if(isGround&&(Input.GetKeyDown("up")||Input.GetKeyDown(KeyCode.Space)&&moveFlag)){
 
             rb.AddForce(transform.up * jumpSpeed, ForceMode2D.Impulse);
             isGround = false;
         }
     }
+
+    void WaitProcess(){
+        if(fieldFlag && transform.position.x >= 240){
+            fieldFlag = false;
+            moveFlag = false;
+        }
+        if(!moveFlag){
+            Debug.Log("動けない");
+            if(_camera.GetComponent<CameraController_hiroppe>().WaitCamera()){
+                fieldNum++;
+                moveFlag = true;
+            }
+        }
+        
+    }
+
+
 
     void OnCollisionEnter2D(Collision2D col){
         if(col.gameObject.tag == "Field"){
--- Assets/Scripts/TilemapManager_hiroppe.cs
+++ Assets/Scripts/TilemapManager_hiroppe.cs
@@ -7,11 +7,20 @@ public class TilemapManager_hiroppe : MonoBehaviour
     public GameObject[] walls;
     public GameObject[] traps;
 
+    private float r; 
     void Start(){
-        
-    }
+        r = Random.Range(0,2);
+        //Debug.Log(r);
 
-    void Update(){
-        
+        for(int i=0;i<3;i++){
+            if(i==r){
+                walls[i].SetActive(true);
+                traps[i].SetActive(true);
+            }
+            else{
+                walls[i].SetActive(false);
+                traps[i].SetActive(false);
+            }
+        }
     }
 }
