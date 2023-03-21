2deb8455e112e64d4b20ca1001d4e23219dc62d5
Ebisyu-mai <shion.uikiti.14@gmail.com>
2023-03-21 16:02:23 +0900

    Plyaerの動作とアニメーションの追加

---
+++ Assets/Scripts/CameracController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameracController : MonoBehaviour
+{
+    public GameObject player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform playerPos = player.transform;
+        Vector2 worldPos = playerPos.position;
+        Vector2 vewPos = Camera.main.WorldToScreenPoint(worldPos);
+        if (vewPos.x > 1000)
+        {
+            this.transform.position += new Vector3(0.01f, 0, 0);
+        }
+        else if (vewPos.x < 500 & worldPos.x > -4)
+        {
+            this.transform.position += new Vector3(-0.01f, 0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public bool ground;
+    private Animator walk;
+    private Animator dead;
+    public bool gameOver;
+    public bool gameOverJump;
+    public int gameOverJumpForce;
+    public BoxCollider2D box;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        ground = false;
+        walk = GetComponent<Animator>();
+        dead = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) & ground & !gameOver)
+        {
+            rb.AddForce(Vector2.up * 300);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) & !gameOver)
+        {
+            walk.SetBool("BlWalk", true);
+            transform.localScale = new Vector3(3, 3, 3);
+            transform.position += new Vector3(0.01f, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow) & !gameOver)
+        {
+            walk.SetBool("BlWalk", true);
+            transform.localScale = new Vector3(-3, 3, 3);
+            transform.position += new Vector3(-0.01f, 0, 0);
+        }
+        else
+        {
+            walk.SetBool("BlWalk", false);
+        }
+
+        if (transform.position.y < -3 & !gameOverJump)
+        {
+            gameOverJumpForce = 700;
+            GameOverAction();
+        }
+
+        if (transform.position.y < -7)
+        {
+            rb.isKinematic = true;
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            ground = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            ground = true;
+        }
+
+        if (collision.gameObject.tag == "FallBlock")
+        {
+            gameOverJumpForce = 300;
+            GameOverAction();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Car")
+        {
+            gameOverJumpForce = 250;
+            GameOverAction();
+        }
+
+        if (collision.gameObject.tag == "BrokenCar")
+        {
+            gameOverJumpForce = 350;
+            GameOverAction();
+        }
+    }
+
+    void GameOverAction()
+    {
+        box.enabled = false;
+        gameOver = true;
+        dead.SetBool("BlDead", true);
+        rb.AddForce(Vector2.up * gameOverJumpForce);
+        gameOverJump = true;
+    }
+}
