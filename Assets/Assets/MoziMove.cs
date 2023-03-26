3f9b9befb497a2e8df20d937d161387801ea0668
shibasakura <ikeshiba0416@gmail.com>
2023-03-26 21:25:38 +0900

    プロジェクト持ってきた

---
+++ Assets/Assets/MoziMove.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoziMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(-2,0,0);
+    }
+}
