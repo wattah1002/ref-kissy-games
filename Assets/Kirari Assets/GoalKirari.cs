e7728dcb8df9f6e4a03146fd375cf4b60c8bb179
青山希輝 <kirari@aoyamanozomiakiranoMacBook-Pro.local>
2023-03-31 03:44:40 +0900

    名前被りを修正

--- Assets/Kirari Assets/GoalKirari.cs
+++ Assets/Kirari Assets/GoalKirari.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Goal : MonoBehaviour
+public class GoalKirari : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/PlayerMoveKirari.cs
+++ Assets/Scripts/PlayerMoveKirari.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class PlayerMove : MonoBehaviour
+public class PlayerMoveKirari : MonoBehaviour
 {
     public Rigidbody2D rb;
     public bool Grounded;
