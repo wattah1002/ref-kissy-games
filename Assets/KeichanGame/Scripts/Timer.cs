d59ddb9830b1c647099e9c04f63e764e546f9bce
keichange <keiichi.kashy@gmail.com>
2023-03-30 20:48:18 +0900

    BGMとSEを入れた

--- Assets/KeichanGame/Scripts/Timer.cs
+++ Assets/KeichanGame/Scripts/Timer.cs
@@ -24,7 +24,6 @@ public class Timer : MonoBehaviour
         remainingTime = timeLimit;
         title.SetActive(false);
         retry.SetActive(false);
-        audioSource.Play();
     }
 
     // Update is called once per frame
