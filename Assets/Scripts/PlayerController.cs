0f6f212ca6ad0efa229eba5d69e90a784776386d
Ebisyu-mai <shion.uikiti.14@gmail.com>
2023-03-30 15:23:24 +0900

    ゲームクリアのプログラムの作成とステージ調整

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public SpriteRenderer playerSprite;
     public Rigidbody2D rb;
     public bool ground;
     private Animator walk;
@@ -12,51 +13,74 @@ public class PlayerController : MonoBehaviour
     public bool gameOverJump;
     public int gameOverJumpForce;
     public BoxCollider2D box;
+    public bool goal;
+    public bool clear;
     void Start()
     {
+        playerSprite = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
         ground = false;
         walk = GetComponent<Animator>();
         dead = GetComponent<Animator>();
+        transform.position = new Vector3(-2.2f, -2, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) & ground & !gameOver)
-        {
-            rb.AddForce(Vector2.up * 300);
-        }
 
-        if (Input.GetKey(KeyCode.RightArrow) & !gameOver)
+        if (!goal)
         {
-            walk.SetBool("BlWalk", true);
-            transform.localScale = new Vector3(3, 3, 3);
-            transform.position += new Vector3(0.01f, 0, 0);
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow) & !gameOver)
-        {
-            walk.SetBool("BlWalk", true);
-            transform.localScale = new Vector3(-3, 3, 3);
-            transform.position += new Vector3(-0.01f, 0, 0);
-        }
+            if (Input.GetKeyDown(KeyCode.Space) & ground & !gameOver)
+            {
+                rb.AddForce(Vector2.up * 300);
+            }
+
+            if (Input.GetKey(KeyCode.RightArrow) & !gameOver)
+            {
+                walk.SetBool("BlWalk", true);
+                transform.localScale = new Vector3(3, 3, 3);
+                transform.position += new Vector3(0.01f, 0, 0);
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow) & !gameOver)
+            {
+                walk.SetBool("BlWalk", true);
+                transform.localScale = new Vector3(-3, 3, 3);
+                transform.position += new Vector3(-0.01f, 0, 0);
+            }
+            else
+            {
+                walk.SetBool("BlWalk", false);
+            }
+
+            if (transform.position.y < -4 & !gameOverJump)
+            {
+                gameOverJumpForce = 400;
+                rb.velocity = Vector3.zero;
+                GameOverAction();
+            }
+
+            if (transform.position.y < -7)
+            {
+                rb.isKinematic = true;
+                rb.velocity = Vector2.zero;
+            }
+        } 
         else
         {
-            walk.SetBool("BlWalk", false);
+            if (transform.position.x < 173)
+            {
+                walk.SetBool("BlWalk", true);
+                transform.localScale = new Vector3(3, 3, 3);
+                transform.position += new Vector3(0.003f, 0, 0);
+            }
+            else
+            {
+                playerSprite.enabled = false;
+                clear = true;
+            }
         }
-
-        if (transform.position.y < -4 & !gameOverJump)
-        {
-            gameOverJumpForce = 400;
-            rb.velocity= Vector3.zero;
-            GameOverAction();
-        }
-
-        if (transform.position.y < -7)
-        {
-            rb.isKinematic = true;
-            rb.velocity = Vector2.zero;
-        }
+        
     }
 
     private void OnCollisionExit2D(Collision2D collision)
@@ -98,6 +122,11 @@ public class PlayerController : MonoBehaviour
             gameOverJumpForce = 300;
             rb.velocity = Vector3.zero;
             GameOverAction();
+        }
+
+        if (collision.gameObject.tag == "Goal")
+        {
+            goal = true;
         }
     }
 
